72c4854d8980f2db195dc49fb1b5f0ce38c9b621
john pinkston <pinkstonjack2@gmail.com>
2016-12-22 13:42:06 -0600

    MVP

--- app/controllers/ProfCtrl.js
+++ app/controllers/ProfCtrl.js
@@ -1,5 +1,27 @@
 "use strict";
 
 app.controller('ProfCtrl', function($scope, ProfFactory) {
-	ProfFactory.getUserStats();
+
+	ProfFactory.getUserStats()
+	.then((stats) => {
+		$scope.setValues(stats);
+		$scope.setBestTime(stats);
+	}).then(() => {
+		$scope.$apply();
+	});
+
+	$scope.setValues = (stats) => {
+		$scope.userName = stats[0].userName;
+		$scope.totalComplete = stats[0].totalComplete;
+		$scope.totalAbandoned = stats[0].totalAbandoned;
+		$scope.totalAttempts = stats[0].totalAttempts;
+	};
+
+	$scope.setBestTime = (stats) => {
+		let trueTime = stats[0].bestTime / 1000;
+		let minutes = Math.floor(trueTime / 60);
+		let seconds = (trueTime % 60).toPrecision(2);
+		$scope.bestTime = minutes + ":" + seconds;
+	};
+
 });
